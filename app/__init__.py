e82e645df0b6a6042253bdb8d7ca80eeec2e2161
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-06-07 22:26:46 +0300

    feat: initial project tree and dependencies

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from dotenv import load_dotenv
+
+
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+    app.config.from_object('app.config.Config')
+
+    with app.app_context():
+        from .routes import routes
+        app.register_blueprint(routes)
+
+    return app
---
+++ app/config.py
@@ -0,0 +1,5 @@
+import os
+
+
+class Config:
+    AES_SECRET_KEY = os.getenv('AES_SECRET_KEY')
