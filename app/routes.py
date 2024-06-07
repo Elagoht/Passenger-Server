8441299026a86f5a97e81e6ff2e1c563238b5ae5
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-06-08 01:27:36 +0300

    feat: registered all required methods

--- app/routes.py
+++ app/routes.py
@@ -17,10 +17,31 @@ def login(): pass
 def register(): pass
 
 
+@routes.route("/reset", methods=["POST"])
+@get_requested_fields
+@auth_required
+@service_route(200, 400, "new")
+def reset(): pass
+
+
 @routes.route("/fetchAll", methods=["GET"])
 @auth_required
 @service_route(200, 404)
 def fetchAll(): pass
+
+
+@routes.route("/query", methods=["POST"])
+@get_requested_fields
+@auth_required
+@service_route(200, 404, "keyword")
+def query(): pass
+
+
+@routes.route("/fetch", methods=["POST"])
+@get_requested_fields
+@auth_required
+@service_route(200, 404, "uuid")
+def fetch(): pass
 
 
 @routes.route("/create", methods=["POST"])
@@ -28,6 +49,58 @@ def fetchAll(): pass
 @auth_required
 @service_route(201, 400, "data")
 def create(): pass
+
+
+@routes.route("/update", methods=["PUT"])
+@get_requested_fields
+@auth_required
+@service_route(200, 400, "uuid", "data")
+def update(): pass
+
+
+@routes.route("/delete", methods=["DELETE"])
+@get_requested_fields
+@auth_required
+@service_route(200, 404, "uuid")
+def delete(): pass
+
+
+@routes.route("/stats", methods=["GET"])
+@auth_required
+@service_route(200, 404)
+def stats(): pass
+
+
+@routes.route("/declare", methods=["POST"])
+@get_requested_fields
+@auth_required
+@service_route(201, 400, "key", "value")
+def declare(): pass
+
+
+@routes.route("/forget", methods=["DELETE"])
+@get_requested_fields
+@auth_required
+@service_route(200, 404, "key")
+def forget(): pass
+
+
+@routes.route("/constants", methods=["GET"])
+@auth_required
+@service_route(200, 404)
+def constants(): pass
+
+
+@routes.route("/generate", methods=["POST"])
+@get_requested_fields
+@service_route(200, 400, "length")
+def generate(): pass
+
+
+@routes.route("/manipulate", methods=["POST"])
+@get_requested_fields
+@service_route(200, 400, "passphrase")
+def manipulate(): pass
 
 
 def init_app(app):
