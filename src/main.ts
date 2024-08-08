57e61ac6d08ba21df7c5df126bac878157a08614
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-08-08 14:16:12 +0300

    feat: swagger documentation added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./passenger.module"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 async function bootstrap() {
   const passenger = await NestFactory.create(AppModule)
+
+  const document = SwaggerModule.createDocument(
+    passenger,
+    new DocumentBuilder()
+      .setTitle("Passenger Server")
+      .setDescription("An HTTP translator for Passenger Core CLI")
+      .setVersion("0.3.0-beta.1")
+      .setExternalDoc("Website", "https://passenger-landing.vercel.app/")
+      .setLicense("GPL-3.0", "https://www.gnu.org/licenses/gpl-3.0.html")
+      .addBearerAuth()
+      .build()
+  )
+  SwaggerModule.setup("swagger", passenger, document)
+
   await passenger.listen(3000)
 }
 bootstrap()
